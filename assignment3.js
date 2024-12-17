3d485de1439cf67a65e961a28b9ce8a3f8935f4a
lavondedunigan <93886681+lavondedunigan@users.noreply.github.com>
2024-12-17 14:19:14 -0600

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,11 @@
+function convertCToF(celcius) {
+    var fahrenheit;
+    // Only change code below this line
+    let fahrenheit = celsius * 9/5 + 32;
+
+    // Only change the code above this line 
+    return fahrenheit;
+    
+}
+ // Change the inputs below to test code
+(convertCToF());
